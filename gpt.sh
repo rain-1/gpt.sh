163486f53de6095eee6984d9c0cbcf632c5590cc
rain1 <rain1@airmail.cc>
2023-04-16 07:30:03 +0100

    Update gpt.sh

--- gpt.sh
+++ gpt.sh
@@ -21,7 +21,7 @@ curl https://api.openai.com/v1/completions \
   -o "${tmpfile}" \
   -H "Content-Type: application/json" \
   -H "Authorization: Bearer $OPENAI_API_KEY" \
-  -d "{\"model\": \"text-davinci-003\", \"prompt\": \"$preprompt$args\", \"max_tokens\": 256, \"temperature\": 0}"
+  -d "{\"model\": \"gpt-3.5-turbo\", \"prompt\": \"$preprompt$args\", \"max_tokens\": 256, \"temperature\": 0}"
 if [ $? -eq 0 ]
 then
   result="$(jq -r '.choices[0].text' "${tmpfile}")"
